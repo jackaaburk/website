cb1c0d47b6fea79662a2d477ca793d39f114cc2b
jackaaburk <jack.aa.burk@gmail.com>
2023-12-14 14:43:57 -0500

    done

---
+++ pages/webvilletunes/playlist.js
@@ -0,0 +1,23 @@
+function setUpButtonHandler() {
+    let button = document.getElementById("addButton");
+    button.onclick = handleButtonClick;
+}
+
+
+function handleButtonClick() {
+    let textInput = document.getElementById("songTextInput");
+    let songName = textInput.value;
+    if (songName == "") {
+        alert("Please enter a song!");
+    }
+    else {
+       console.log("Adding " + songName);
+        let li = document.createElement("li");
+		li.innerHTML = songName;
+		let ul = document.getElementById("playlist");
+		ul.appendChild(li);
+	      ul.style.backgroundColor = "crimson";
+	      ul.style.color = "black";
+	    save(songName);
+    }
+}
